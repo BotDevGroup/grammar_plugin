dce6310536d97e63e364d8b3fa190d88e0bfd744
Ricardo Arturo Cabral <ricardo.arturo.cabral@gmail.com>
2018-06-17 12:28:53 -0400

    Initial commit

---
+++ README.rst
@@ -0,0 +1,40 @@
+===============================
+Marvinbot Grammar Plugin
+===============================
+
+
+.. image:: https://img.shields.io/pypi/v/grammar_plugin.svg
+        :target: https://pypi.python.org/pypi/grammar_plugin
+
+.. image:: https://img.shields.io/travis/Cameri/grammar_plugin.svg
+        :target: https://travis-ci.org/Cameri/grammar_plugin
+
+.. image:: https://readthedocs.org/projects/grammar-plugin/badge/?version=latest
+        :target: https://grammar-plugin.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/Cameri/grammar_plugin/shield.svg
+     :target: https://pyup.io/repos/github/Cameri/grammar_plugin/
+     :alt: Updates
+
+
+A plugin for marvinbot to check grammar using Languagetool
+
+
+* Free software: MIT license
+* Documentation: https://grammar-plugin.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
---
+++ grammar_plugin/base.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from marvinbot.plugins import Plugin
+from marvinbot.handlers import CommandHandler
+import logging
+import requests
+import json
+
+log = logging.getLogger(__name__)
+
+
+class GrammarPlugin(Plugin):
+    def __init__(self):
+        super(GrammarPlugin, self).__init__('grammar_plugin')
+        self.config = None
+
+    def get_default_config(self):
+        return {
+            'short_name': self.name,
+            'enabled': True,
+            'base_url': 'https://languagetool.org/api/v2/check',
+            'language': 'auto',
+            'preferredLanguages': 'en-US,en-GB'
+        }
+
+    def configure(self, config):
+        self.config = config
+
+    def setup_handlers(self, adapter):
+        log.info("Setting up handlers for Grammar plugin")
+        self.add_handler(CommandHandler('grammar', self.on_grammar_command,
+                                        command_description='Checks the grammar on the text being replied to.'))
+
+    def setup_schedules(self, adapter):
+        pass
+
+    def on_grammar_command(self, update):
+        message = update.effective_message
+
+        if message.reply_to_message is None:
+            message.reply_text(text='❌ When checking grammar, use this command while replying.')
+
+        text = message.reply_to_message.text
+        try:
+          data = self.fetch_corrections(text)
+        except:
+            message.reply_text(text='❌ An error occurred while checking the grammar')
+            return
+
+        log.info(json.dumps(data))
+
+        responses = []
+        curr_len = 0
+        for match in data.get('matches'):
+            context = match.get('context', {}).get('text', '')
+            offset = match.get('context', {}).get('offset', 0)
+            length = match.get('context', {}).get('length', 0)
+            decorated = '{}<b>{}</b>{}'.format(
+                context[:offset],
+                context[offset:offset + length],
+                context[offset + length:]
+            )
+            suggestions = ', '.join([kvp.get('value', '') for kvp in match.get('replacements', [])[:5]])
+            response = '✏️ <b>{}</b>\n{}'.format(match.get('message'), decorated)
+            if len(suggestions) > 0:
+                response += '\n<b>Suggestions:</b> {}'.format(suggestions)
+
+            responses.append(response)
+
+        message.reply_text(text='\n\n'.join(responses)[:4096], parse_mode='HTML')
+
+    def fetch_corrections(self, text):
+        payload = {
+            'text': text,
+            'language': self.config.get('language'),
+            'preferredLanguages': self.config.get('preferredLanguages')
+        }
+        url = self.config.get('base_url')
+        response = requests.post(url, data=payload)
+        return response.json()
